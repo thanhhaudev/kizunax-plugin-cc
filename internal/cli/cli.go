fd465e33345f0c95ee18a4acd485973ff42d6083
thanhhaudev <thanhhaudev@gmail.com>
2026-06-03 00:14:34 +0700

    v0.10.0: provider routing, anthropic default, token guard, render fixes

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -6,7 +6,7 @@ import (
 	xerrors "github.com/thanhhaudev/kizunax-plugin-cc/internal/errors"
 )
 
-const Version = "0.9.0"
+const Version = "0.10.0"
 
 func Dispatch(args []string) error {
 	if len(args) == 0 {
