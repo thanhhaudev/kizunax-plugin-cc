a6d35e84f44871ab76e6f99aa97189a469e82bf2
thanhhaudev <thanhhaudev@gmail.com>
2026-06-04 12:53:36 +0700

    chore: bump version to 0.12.4

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -6,7 +6,7 @@ import (
 	xerrors "github.com/thanhhaudev/kizunax-plugin-cc/internal/errors"
 )
 
-const Version = "0.12.3"
+const Version = "0.12.4"
 
 func Dispatch(args []string) error {
 	if len(args) == 0 {
